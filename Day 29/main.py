47240a9236944ab470cd8bcf561b80a4c8fd068f
Marek Kożdoń <mkozdon@o2.pl>
2025-04-18 12:20:44 +0200

    Added Search functionality to load existing password from JSON

--- Day 29/main.py
+++ Day 29/main.py
@@ -58,6 +58,17 @@ def save_pwd():
         website_entry.delete(0, "end")
 
 
+def search():
+    website = website_entry.get()
+    with open(STORAGE_PATH, "r") as data_file:
+        data = json.load(data_file)
+    if website in data:
+        message = f"Email: {data[website]['username']} \nPassword: {data[website]['password']}"
+    else:
+        message = f"Data for {website} not found."
+    mb.showwarning(title=website, message=message)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 window = tk.Tk()
 
@@ -71,23 +82,26 @@ canvas.create_image(100, 100, image=logo)
 website_label = tk.Label(text="Website:")
 username_label = tk.Label(text="Email/Username:")
 pwd_label = tk.Label(text="Password:")
-website_entry = tk.Entry(width=40)
+website_entry = tk.Entry(width=21)
 username_entry = tk.Entry(width=40)
 pwd_entry = tk.Entry(width=21)
-generate_button = tk.Button(text="Generate Password", command=generate_password_click)
+generate_button = tk.Button(
+    text="Generate Password", command=generate_password_click, width=17
+)
 add_button = tk.Button(text="Add", width=39, command=save_pwd)
-
+search_button = tk.Button(text="Search", width=17, command=search)
 
 canvas.grid(column=0, row=0, columnspan=3)
 website_label.grid(column=0, row=1)
 username_label.grid(column=0, row=2)
 pwd_label.grid(column=0, row=3)
-website_entry.grid(column=1, row=1, columnspan=2)
+
+website_entry.grid(column=1, row=1, sticky="w")
 username_entry.grid(column=1, row=2, columnspan=2)
 pwd_entry.grid(column=1, row=3, sticky="w")
 generate_button.grid(column=2, row=3, sticky="e")
 add_button.grid(column=1, row=4, columnspan=2)
-
+search_button.grid(column=2, row=1, sticky="e")
 
 window.mainloop()
 # columnspan = 2
